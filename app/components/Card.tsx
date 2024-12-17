8ae9c0dcdab510380ddfb8ee58fd64a7e3ea8847
IbrahimKo95 <ibrahkonate95@gmail.com>
2024-12-17 22:35:31 +0100

    add: type badge on card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -13,11 +13,14 @@ export default function Card(props: {data: Movie}) {
     }, [props.data]);
     return (
         <a href={`/${dataType}/${props.data.id}`}>
-            <div className="relative w-52 transition-all duration-300 group">
+            <div className="relative w-52 transition-all duration-300 group rounded-xl">
                 <img src={`https://image.tmdb.org/t/p/w500${props.data.poster_path}`} alt={props.data.title}
-                     className="w-full h-auto mb-3 group-hover:brightness-50 "/>
+                     className="w-full h-auto mb-3 group-hover:brightness-50 rounded-xl"/>
+                <div className="absolute top-5 left-5">
+                    <Badge className={`rounded-full text-sm ${dataType === "serie" ? "bg-purple-800" : ""}`}>{dataType === "movie" ? "FILM" : "SERIE"}</Badge>
+                </div>
                 <div className="opacity-0 absolute left-[-5px] bottom-5 group-hover:left-5 group-hover:opacity-100 text-lg transition-all duration-300 delay-100">
-                    <p>{props.data.title}</p>
+                    <p className="font-bold">{props.data.title}</p>
                     <div className="flex gap-x-3">
                         {props.data.release_date ? (
                             <Badge>{props.data.release_date}</Badge>
